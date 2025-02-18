dd3c925da2c3a3eaf5004f6c80d002b47aec5a6a
anower77 <anowerhossain765562@gmail.com>
2025-02-18 19:54:51 +0600

    deploy v.1.0.3

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py makemigrations
+python manage.py migrate 
