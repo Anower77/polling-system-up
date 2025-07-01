2d123a965fb5996d81b351bcf92680458271da67
anower77 <anowerhossain765562@gmail.com>
2025-07-01 18:05:36 +0600

    CI/CD Setup v.0.1

--- polling/settings.py
+++ polling/settings.py
@@ -86,11 +86,11 @@ ASGI_APPLICATION = 'polling.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'NAME': env('DB_NAME', default='polling_db'),
+        'USER': env('DB_USER', default='postgres'),
+        'PASSWORD': env('DB_PASSWORD', default='password'),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default='5432'),
         'OPTIONS': {
             'sslmode': 'require'
         }
